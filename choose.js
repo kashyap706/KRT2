ac3c6bb1b24a98cc0c1712f9423a37be04f10a7a
kashyap706 <kashyapthakkar2605@gmail.com>
2024-08-25 16:17:59 +0530

    Add files via upload

---
+++ choose.js
@@ -0,0 +1,20 @@
+function addition()
+ {
+    window.location.replace("choose.html");
+}
+
+function subtraction()
+ {
+    window.location.replace("choose.html");
+}
+
+function division()
+ {
+    window.location.replace("choose.html");
+}
+
+function multiplication()
+ {
+    window.location.replace("choose.html");
+}
+
